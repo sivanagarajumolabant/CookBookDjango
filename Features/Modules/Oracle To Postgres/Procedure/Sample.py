6b981f1f5883cd80ab20da35bb8e9da9ccebd484
siva <siva.n@quadrantresource.com>
2022-02-11 21:05:27 +0530

    chnages

--- Features/Modules/Oracle To Postgres/Procedure/Sample.py
+++
@@ -1,3 +0,0 @@
-def Sample(data):
-   data = data.replace("sample","sample ex")
-   return data
--- Features/Modules/Oracle To Postgres/Procedure/language1.py
+++
@@ -1,3 +0,0 @@
-def language1(data):
-    data = data.replace('language_quadrant', 'language')
-    return data
