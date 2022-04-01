487ea34427cb046aeeea0f45ac797fe5170d2cf3
siva <siva.n@quadrantresource.com>
2022-04-01 23:15:33 +0530

    chnages

--- Features/settings.py
+++ Features/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'Features.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qbook',
+        'USER': 'qmigdmg',
+        'PASSWORD': 'Password123$',
+        'HOST': 'qmignewdmg.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
